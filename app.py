6e0883c45f5e4873cff757d2fcce201f43b468a6
YUvraj <yuvrajverma9890@gmail.com>
2025-03-31 19:43:04 +0530

    pagination

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Query
 from pymongo import MongoClient
 
 app = FastAPI()
@@ -10,17 +10,21 @@ risk_collection = db["fire_risk_predictions"]
 live_fire = db["weather_data"]
 
 @app.get("/fire-risk")
-async def get_fire_risk_data():
+async def get_fire_risk_data(page: int = Query(1, alias="page"), limit: int = Query(50, alias="limit")):
     """Fetch all fire risk data from MongoDB."""
-    
-    fire_risk_data = list(risk_collection.find({}, {"_id": 0}))  # Exclude _id field
-    return fire_risk_data
+    skip = (page - 1) * limit  # Calculate offset
+    fire_risk_data = list(risk_collection.find({}, {"_id": 0}).skip(skip).limit(limit))  # Exclude _id field
+    return {"data":fire_risk_data , "page": page, "limit": limit}
 
 
 @app.get("/live-fire")
-async def get_fire_risk_data():
-    """Fetch all live fire data from MongoDB."""
-    
-    live_fire_data = list(live_fire.find({}, {"_id": 0}))  # Exclude _id field
-    return live_fire_data
+async def get_fire_risk_data(page: int = Query(1, alias="page"), limit: int = Query(50, alias="limit")):
+    """
+    Fetch live fire data from MongoDB with pagination.
+    - `page`: Page number (default: 1)
+    - `limit`: Number of records per page (default: 50)
+    """
+    skip = (page - 1) * limit  # Calculate offset
 
+    live_fire_data = list(live_fire.find({}, {"_id": 0}).skip(skip).limit(limit))  # Pagination applied
+    return {"data": live_fire_data, "page": page, "limit": limit}
